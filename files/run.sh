3baa7c1597cc194d5234986bbd964fad2cc9d0d6
Mitchell Hewes <me@mitcdh.com>
2016-09-21 11:16:19 +1000

    Added support for traversing symlinks in /www

--- files/run.sh
+++ files/run.sh
@@ -3,7 +3,7 @@
 chmod -R +x /scripts/pre-run/ 
 run-parts --exit-on-error /scripts/pre-run/ || exit $?
 
-chown -R web-srv:www-data /www
+chown -RL web-srv:www-data /www
 chown -R web-srv:www-data /etc/hiawatha/conf.d
 
 exec hiawatha -d
