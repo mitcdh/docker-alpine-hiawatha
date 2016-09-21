ed7c024bd42021fd198b43148a455e854a11cc96
Mitchell Hewes <me@mitcdh.com>
2016-09-21 11:53:36 +1000

    removed symlink support

--- files/run.sh
+++ files/run.sh
@@ -3,7 +3,7 @@
 chmod -R +x /scripts/pre-run/ 
 run-parts --exit-on-error /scripts/pre-run/ || exit $?
 
-chown -RL web-srv:www-data /www
+chown -R web-srv:www-data /www
 chown -R web-srv:www-data /etc/hiawatha/conf.d
 
 exec hiawatha -d
